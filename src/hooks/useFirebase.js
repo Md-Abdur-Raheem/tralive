000c5e3e634d63850aeca466c8ea8e620c29087e
Md-Abdur-Raheem <md.raheem2450@gmail.com>
2021-10-30 17:30:47 +0600

    firebase deployed

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -14,7 +14,6 @@ const useFirebase = () => {
         signInWithPopup(auth, googleProvider)
             .then(result => {
                 const user = result.user;
-                console.log(user);
                 setUser(user);
             })
             .catch(error => {
